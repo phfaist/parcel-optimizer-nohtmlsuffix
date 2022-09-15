b973dc962b190164f0f048da59fe9fa4a658ff37
Philippe Faist <phfaist@gmail.com>
2022-09-15 21:45:43 +0200

    support #fragments in URL

--- index.js
+++ index.js
@@ -10,38 +10,43 @@ const urls = require('posthtml-urls');
 
 // Configure.
 const stripIndexHtml = (startsWith, url) => {
-  // Verify the URL is our own.
-  if (url.indexOf(startsWith) !== 0) {
-    // original URL
-    return url;
-  }
-  if (url.slice(-11) === '/index.html') {
-    return url.slice(0, -10); // Keep trailing slash.
-  }
-  if (url.slice(-5) === '.html') {
-    return url.slice(0, -5); // strip trailing .html suffix from all local pages
-  }
-  return url; // Return original URL.
+    // Verify the URL is our own.
+    if (url.indexOf(startsWith) !== 0) {
+        // original URL
+        return url;
+    }
+
+    let fragment = '';
+    let fragment_idx = url.indexOf('#');
+    if (fragment_idx !== -1) {
+        fragment = url.slice(fragment_idx);
+        url = url.slice(0, fragment_idx);
+    }
+
+    if (url.slice(-11) === '/index.html') {
+        return url.slice(0, -10) + fragment; // Keep trailing slash.
+    }
+    if (url.slice(-5) === '.html') {
+        return url.slice(0, -5) + fragment; // strip trailing .html suffix from all local pages
+    }
+    return url + fragment; // Return original URL.
 };
+
 
 // Exports.
 module.exports = new Optimizer({
-  async optimize({
-    bundle,
-    contents,
-    map,
-    options,
-  }) {
-    // Disable in hot mode because wrong index.html might be served.
-    // @see https://github.com/parcel-bundler/parcel/issues/4740
-    if (options.hot) {
-      return { contents, map };
-    }
+    async optimize({ bundle, contents, map, options, })
+    {
+        // Disable in hot mode because wrong index.html might be served.
+        // @see https://github.com/parcel-bundler/parcel/issues/4740
+        if (options.hot) {
+            return { contents, map };
+        }
 
-    const { publicUrl } = bundle.target;
-    const plugin = urls({ eachURL: stripIndexHtml.bind(null, publicUrl) });
-    return {
-      contents: (await posthtml([plugin]).process(contents)).html,
-    };
-  },
+        const { publicUrl } = bundle.target;
+        const plugin = urls({ eachURL: stripIndexHtml.bind(null, publicUrl) });
+        return {
+            contents: (await posthtml([plugin]).process(contents)).html,
+        };
+    },
 });
